83550eb2f145358407a832313b26ee13d3a3883f
Oleh M <oleh.mesh@gmail.com>
2020-06-27 21:28:07 +0300

    fixing DataModule from DI

--- repository/src/main/java/com/olehmesh/repository/di/module/DataModule.kt
+++ repository/src/main/java/com/olehmesh/repository/di/module/DataModule.kt
@@ -12,13 +12,16 @@ import dagger.Provides
 @Module
 class DataModule(var context: Context) {
 
-    private var jsonObj = Gson()
-    private val arrayType = object : TypeToken<List<InstCellModel>>() {}.type
+    @DataScope
+    @Provides
+    fun provideJson(): Gson {
+        return Gson()
+    }
 
     @DataScope
     @Provides
-    fun provideDataFromJson(): MutableList<InstCellModel?> {
-
+    fun provideDataFromJson(jsonObj: Gson): MutableList<InstCellModel?> {
+        val arrayType = object : TypeToken<List<InstCellModel>>() {}.type
         val br =
             context.resources.openRawResource(R.raw.data).bufferedReader()
                 .use {
